a6a1853c97c2eb4bc28b521780f1403ad34b4a7b
Diego Mejías López <diego.mejias.contractor@bbva.com>
2021-10-27 14:24:18 +0200

    Primera ejecucion heroku

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,7 +10,8 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class ContactComponent implements OnInit {
 
 	form: FormGroup;
-	name: FormControl = new FormControl("", [Validators.required]);
+	firstName: FormControl = new FormControl("", [Validators.required]);
+	lastName: FormControl = new FormControl("", [Validators.required]);
 	email: FormControl = new FormControl("", [Validators.required, Validators.email]);
 	message: FormControl = new FormControl("", [Validators.required, Validators.maxLength(256)]);
 	honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
@@ -19,7 +20,8 @@ export class ContactComponent implements OnInit {
 	responseMessage: string; // the response message to show to the user
 	constructor(private formBuilder: FormBuilder, private http: HttpClient) {
 		this.form = this.formBuilder.group({
-			name: this.name,
+			firstName: this.firstName,
+			lastName: this.lastName,
 			email: this.email,
 			message: this.message,
 			honeypot: this.honeypot
@@ -31,12 +33,13 @@ export class ContactComponent implements OnInit {
 		if (this.form.status == "VALID" && this.honeypot.value == "") {
 			this.form.disable(); // disable the form if it's valid to disable multiple submissions
 			var formData: any = new FormData();
-			formData.append("name", this.form.get("name").value);
-			formData.append("email", this.form.get("email").value);
-			formData.append("message", this.form.get("message").value);
+			formData.append("First Name", this.form.get("firstName").value);
+			formData.append("Last Name", this.form.get("lastName").value);
+			formData.append("Email", this.form.get("email").value);
+			formData.append("Message", this.form.get("message").value);
 			this.isLoading = true; // sending the post request async so it's in progress
 			this.submitted = false; // hide the response message on multiple submits
-			this.http.post("YOUR GOOGLE WEB APP URL HERE", formData).subscribe(
+			this.http.post("https://script.google.com/macros/s/AKfycbxKfyUgoTDVQWL5qTaZCuk7GmTPp5Q9V74ao6SNNvOefYMC80suxCBgjwg83ECtgS9N/exec", formData).subscribe(
 				(response) => {
 					// choose the response message
 					if (response["result"] == "success") {
@@ -44,9 +47,10 @@ export class ContactComponent implements OnInit {
 					} else {
 						this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
 					}
-					this.form.enable(); // re enable the form after a success
+					
 					this.submitted = true; // show the response message
 					this.isLoading = false; // re enable the submit button
+					this.form.enable(); // re enable the form after a success
 					console.log(response);
 				},
 				(error) => {
